738ac110b6a61a71e4390ee11b3cadd841e144b5
ik57 <10783856+ik57@users.noreply.github.com>
2024-12-02 23:35:21 +0300

    Add adventofcode2024 day one part one rs file

--- aoc24/day_one/src/main.rs
+++ aoc24/day_one/src/main.rs
@@ -6,5 +6,22 @@
 // What is the total distance between your lists?
 
 fn main() {
-
+    let mut left_loc: Vec<i32> = Vec::new();
+    let mut right_loc: Vec<i32> = Vec::new();
+    
+    for input in include_str!("input.txt").lines() {
+        for line in input.lines(){
+            let mut x = line.split_whitespace();
+            left_loc.push(x.next().unwrap().parse::<i32>().unwrap(),);
+            right_loc.push(x.next().unwrap().parse::<i32>().unwrap(),);
+        }
+    }
+    right_loc.sort();
+    left_loc.sort();
+    
+    let mut answer: i32 = 0;
+    for i in 0..left_loc.len(){
+         answer += (right_loc[i] - left_loc[i]).abs();
+    }
+    println!("The puzzle answer: {answer}");
 }
