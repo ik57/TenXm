a4f47e8d0affc319df629e6628c64c12166f2fc7
ik57 <10783856+ik57@users.noreply.github.com>
2024-12-02 20:26:48 +0300

    create adventofcode2024 day one part one

---
+++ aoc24/day_one/src/main.rs
@@ -0,0 +1,10 @@
+// Day one, part one.
+// https://adventofcode.com/2024/day/1
+// 1) Pair up the smallest number in the left list with the smallest number in the right list.
+// 2) Find the total distance between the left list and the right list in 'input.txt'. 
+// 3) Add up the distances between all of the pairs you found.
+// What is the total distance between your lists?
+
+fn main() {
+
+}
